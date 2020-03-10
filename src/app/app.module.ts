237c0bb3315df1bb9aee3811b6d2a23f64e026d0
SoloKyo <kyokitisin@gmail.com>
2020-03-10 23:54:43 +0900

    Add material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { Division2BuilderModule } from './division2-builder/division2-builder.module'; 
+import { MaterialModule} from './material/material.module'
+import { Division2BuilderModule } from './division2-builder/division2-builder.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule, Division2BuilderModule,
+    BrowserModule, Division2BuilderModule, BrowserAnimationsModule, MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
