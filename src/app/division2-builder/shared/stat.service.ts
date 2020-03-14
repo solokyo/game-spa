435105c722190b1dba128a857852ecac9b809527
SoloKyo <kyokitisin@gmail.com>
2020-03-14 22:52:17 +0900

    create weapons.json

--- src/app/division2-builder/shared/stat.service.ts
+++ src/app/division2-builder/shared/stat.service.ts
@@ -1,21 +1,43 @@
 import { Injectable } from '@angular/core';
 import { DataService } from './data.service';
+import { Weapon } from './types/weapon';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatService {
+  initialStats: any;
   stats: any;
+  stats$: BehaviorSubject<any>;
+  equippedWeapon: Weapon;
   constructor(
     private dataService: DataService
   ) {
-    this.dataService.getStable('statBoardInit','/assets/data.json').subscribe(data => {
-      this.stats = data;
-      console.log(this.stats);
+    this.stats$ = new BehaviorSubject('');
+    this.dataService.getStable('statBoardInit', '/assets/data.json').subscribe(data => {
+      this.initialStats = data;
+      this.updateStats();
     });
   }
 
-  sum(obj) {
+  getStats(): Observable<any> {
+    return this.stats$.asObservable();
+  }
+
+  setEquippedWeapon(equippedWeapon: Weapon): void {
+    this.equippedWeapon = equippedWeapon;
+    this.updateStats();
+  }
+
+  // Get a copy from initial stats then bind all object that contains attributes together and do sum.
+  updateStats(): void {
+    this.stats = { ...this.initialStats };
+    this.sum({ ...this.equippedWeapon, });// ...gearSetsBouns, ...gears, ...specialization
+    this.stats$.next(this.stats);
+  }
+
+  private sum(obj): void {
     Object.keys(obj).forEach(key => {
       if (typeof obj[key] === 'object') {
         if (key === 'bonus') {
@@ -23,17 +45,16 @@ export class StatService {
         }
         this.sum(obj[key]);
       }
-    })
+    });
   }
 
   private sumStats(obj) {
-    obj.forEach(function (bonus) {
+    obj.forEach((bonus) => {
       if (this.stats.hasOwnProperty(bonus.attribute)) {
         this.stats[bonus.attribute] = this.stats[bonus.attribute] + bonus.value;
       } else {
         this.stats[bonus.attribute] = bonus.value;
       }
     });
-    console.log(this.stats);
   }
 }
