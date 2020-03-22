142676677dbff57127db3909fccfbc1b26f1e6ef
SoloKyo <kyokitisin@gmail.com>
2020-03-22 23:56:29 +0900

    update gear selector

--- src/app/division2-builder/gear-selector/gear-selector.component.ts
+++ src/app/division2-builder/gear-selector/gear-selector.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../shared/data.service';
+import { StatService } from '../shared/stat.service';
+import { UtilService } from '../shared/util.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-gear-selector',
+  selector: 'd2b-gear-selector',
   templateUrl: './gear-selector.component.html',
   styleUrls: ['./gear-selector.component.css']
 })
 export class GearSelectorComponent implements OnInit {
-
-  constructor() { }
+  gearSlots: any;
+  constructor(
+    public dialog: MatDialog,
+    private dataService: DataService,
+    private utilService: UtilService,
+    private statService: StatService
+  ) {
+    this.dataService.getStable('gearSlots', '/assets/data.json').subscribe(data => {
+      this.gearSlots = data;
+    });
+  }
 
   ngOnInit(): void {
   }
