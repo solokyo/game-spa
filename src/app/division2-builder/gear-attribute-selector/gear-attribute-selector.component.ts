0c9c4ac602da57cedc5fdd3616fd879ef0ce2f6b
SoloKyo <kyokitisin@gmail.com>
2020-04-06 23:47:49 +0900

    update gear attribute selector
    * add some codes

--- src/app/division2-builder/gear-attribute-selector/gear-attribute-selector.component.ts
+++ src/app/division2-builder/gear-attribute-selector/gear-attribute-selector.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Gear } from '../shared/types/gear';
+import { MatDialog } from '@angular/material/dialog';
+import { ObjectPickerDialogComponent } from '../object-picker-dialog/object-picker-dialog.component';
+import { UtilService } from '../shared/util.service';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'd2b-gear-attribute-selector',
@@ -9,9 +13,32 @@ import { Gear } from '../shared/types/gear';
 export class GearAttributeSelectorComponent implements OnInit {
   @Input() gear: Gear;
   @Output() updated = new EventEmitter<boolean>();
-  constructor() { }
+  gearAttributes: Array<any>;
+  constructor(
+    public dialog: MatDialog,
+    private utilService: UtilService,
+    private dataService: DataService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  selectWeaponMod(avaliableMod: any): void {
+    const dialogRef = this.dialog.open(ObjectPickerDialogComponent, {
+      width: '1080px',
+      data: {
+        key: 'gearMod', value: this.gearAttributes.filter(mod => {
+          return avaliableMod.slot === mod.slot && mod.availableOn.includes(avaliableMod.type);
+        })
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(mod => {
+      console.log(mod);
+      if (mod) {
+        this.utilService.updateOrPush(this.gear.mod, mod, 'slot');
+        this.updated.emit(mod);
+      }
+    });
+  }
 }
