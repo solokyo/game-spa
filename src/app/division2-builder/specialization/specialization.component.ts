a3838afb52cfd935b6f694d871fd5c13cb3b961b
SoloKyo <kyokitisin@gmail.com>
2020-04-14 23:30:56 +0900

    updata specialization
    * add some codes

--- src/app/division2-builder/specialization/specialization.component.ts
+++ src/app/division2-builder/specialization/specialization.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../shared/data.service';
+import { StatService } from '../shared/stat.service';
 
 @Component({
   selector: 'd2b-specialization',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./specialization.component.css']
 })
 export class SpecializationComponent implements OnInit {
-
-  constructor() { }
+  specializations: Array<any>;
+  constructor(
+    private dataService: DataService,
+    private statService: StatService,
+  ) { 
+    this.dataService.getStable('specializations','/assets/specialization.json').subscribe(data => {
+      this.specializations = data;
+    })
+  }
 
   ngOnInit(): void {
   }
