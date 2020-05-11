924f0c5052aab53ac2c2c1a813d0f4a7e8ec9060
SoloKyo <kyokitisin@gmail.com>
2020-05-11 23:33:34 +0900

    daily commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 
 import { MaterialModule} from './material/material.module'
 import { Division2BuilderModule } from './division2-builder/division2-builder.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule, Division2BuilderModule, BrowserAnimationsModule, MaterialModule,
